6321bd1224d2005dce76c075f57708f624839126
Yasmin <yasmin@lit.ifce.edu.br>
2021-01-19 10:54:11 -0300

    test get ip

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd($_SERVER['HTTP_X_FORWARDED_FOR']);
+    dd(request()->server('HTTP_X_FORWARDED_FOR'));
     return view('welcome');
 });
