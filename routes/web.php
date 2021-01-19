6739acab4ab94827be4b56ee7ff0ae019a2825af
Yasmin <yasmin@lit.ifce.edu.br>
2021-01-19 10:08:26 -0300

    test get ip

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-     dd(request()->server('SERVER_ADDR'), request()->server('SERVER_PORT'));
+    dd(request()->server('REMOTE_ADDR'));
     return view('welcome');
 });
