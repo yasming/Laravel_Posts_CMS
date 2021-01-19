689ce18eb119aee3de807b52bdb08dfa13d33acf
Yasmin <yasmin@lit.ifce.edu.br>
2021-01-19 11:23:46 -0300

    get ip

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    dd(file_get_contents("http://ipecho.net/plain"));
+    dd(request()->server('HTTP_X_FORWARDED_FOR'));
     return view('welcome');
 });
