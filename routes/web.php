f35a0472b9ed83c81dcd1e0a431583bb9ea0d77c
Yasmin <yasmin@lit.ifce.edu.br>
2021-01-19 10:16:13 -0300

    test get ip

--- routes/web.php
+++ routes/web.php
@@ -14,18 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    
-    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
-        $ip = $_SERVER['HTTP_CLIENT_IP'];  
-        }  
-    //whether ip is from the proxy  
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-    }  
-    //whether ip is from the remote address  
-    else{  
-            $ip = $_SERVER['REMOTE_ADDR'];  
-    }  
-    dd($ip);
+    dd($_SERVER['HTTP_X_FORWARDED_FOR']);
     return view('welcome');
 });
